4912d525d3d7dfc9bedd93a5d39026a08dcacd92
Muhammad Fakhar <muhammadfakhar004@gmail.com>
2025-01-07 09:15:33 -0800

    Implementing Base authenitcation mechanism

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == 'password'     # We can set it from ENV as well, for now I am making hardcoded here
+    end
+  end
 end
