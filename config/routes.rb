1be013216b12ac7d5b503a5f38deffc45b7ba36a
Muhammad Fakhar <muhammadfakhar004@gmail.com>
2025-01-07 07:47:26 -0800

    Urls: Restful Routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    resources :urls, only: %w[create index show]
+  end
+
+  scope module: 'api' do
+    get '/:short_url', to: 'urls#redirect_short_url'
+  end
 end
